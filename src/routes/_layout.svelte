68edee9332c0f178251b224590abed8aa8cdacc6
Marco Calzada <mcalzada@kssoluciones.com>
2021-07-09 22:53:53 -0500

    Solving css dependencies issues

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,13 +2,16 @@
   import Nav from "../components/Nav.svelte";
   import Header from "../components/Header.svelte";
   import Main from "../components/Main.svelte";
+import Footer from "../components/Footer.svelte";
 
   export let segment;
 </script>
 
-<Header />
+<!--<Header />-->
 <Nav {segment} />
 
 <Main>
   <slot />
 </Main>
+
+<Footer />
--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -1,47 +1,46 @@
 (function($) {
-  'use strict';  
+    'use strict';
 
-  	// Filter
+    // Filter
 
-  	$(document).ready(function(){
-	    $('.buttons').click(function(){
-	      var buttonvalue = $(this).attr('data-filter');
+    $(document).ready(function() {
+        $('.buttons').click(function() {
+            var buttonvalue = $(this).attr('data-filter');
 
-	      if(buttonvalue == 'all'){
-	        $('.image-filter').show('1000');
-	      }
-	      else{
-	        $('.image-filter').not('.'+buttonvalue).hide('200');
-	        $('.image-filter').filter('.'+buttonvalue).show('200');
-	      }
-	      $(this).addClass('active').siblings().removeClass('active');
-	    });
-  	});
+            if (buttonvalue == 'all') {
+                $('.image-filter').show('1000');
+            } else {
+                $('.image-filter').not('.' + buttonvalue).hide('200');
+                $('.image-filter').filter('.' + buttonvalue).show('200');
+            }
+            $(this).addClass('active').siblings().removeClass('active');
+        });
+    });
 
-  	// Testimonial Slider
+    // Testimonial Slider
 
-  	$('#testimonial').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:false,
-        dots:true,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:false,
+    $('#testimonial').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: false,
         animateOut: 'fadeOut',
         animateIn: 'flipInX',
-        responsive:{
-            0:{
-                dots:true,
-                items:1
+        responsive: {
+            0: {
+                dots: true,
+                items: 1
             },
-            600:{
-                dots:true,
-                items:1
+            600: {
+                dots: true,
+                items: 1
             },
-            1000:{
-                dots:true,
-                items:1
+            1000: {
+                dots: true,
+                items: 1
             }
         }
     });
@@ -57,9 +56,4 @@
         controlNav: "thumbnails"
     });
 
-    // Parallax
-
-    var scene = document.getElementById('scene');
-    var parallax = new Parallax(scene);
-
-})(window.jQuery); 
+})(window.jQuery);
